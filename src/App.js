d7e62102966189517c4d2076512e8cfa8c2b43ed
Quang Tran <tquang1708@gmail.com>
2022-03-06 16:18:33 -0500

    make frontend able to communicate with backend

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
-import React from 'react';
+import { React, useState, useEffect } from 'react';
 import './App.css';
 
 function App() {
+    const [testState, setTestState] = useState('');
+
+    useEffect(() => {
+        fetch('http://127.0.0.1:8000/api/test')
+            .then((response) => response.text())
+            .then((text) => setTestState(text))
+            .catch((error) => console.log(error));
+    });
+
     return (
-        <p>Hello, world</p>
+        <p>{ testState }</p>
     );
 }
 
