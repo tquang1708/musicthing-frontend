cb24aba818add5bad6f46722597c9471335187be
Quang Tran <tquang1708@gmail.com>
2022-03-14 09:20:11 -0400

    implement basic frontend for listing

--- src/App.js
+++ src/App.js
@@ -1,19 +1,54 @@
 import { React, useState, useEffect } from 'react';
+import PropTypes from 'prop-types';
 import './App.css';
 
 function App() {
-    const [testState, setTestState] = useState('');
+    const [albumsState, setAlbumsState] = useState([]);
 
     useEffect(() => {
-        fetch('http://127.0.0.1:8000/api/test')
-            .then((response) => response.text())
-            .then((text) => setTestState(text))
+        fetch('http://127.0.0.1:8000/api/list')
+            .then((response) => response.json())
+            .then((data) => setAlbumsState(data.albums))
             .catch((error) => console.log(error));
     });
 
-    return (
-        <p>{ testState }</p>
+    const listAlbums = albumsState.map((album) =>
+        <div key={album.name}>
+            <h3>{album.name}</h3>
+            <p>by {album.album_artist_name}</p>
+            <Songs discs={album.discs} album_artist={album.album_artist_name} />
+        </div>
     );
+
+    return (
+        <div>{ listAlbums }</div>
+    );
+}
+
+function Songs(props) {
+    const { discs, album_artist } = props;
+
+    let listSongs;
+    if (discs.length == 1) {
+        listSongs = discs[0].tracks.map((song) => {
+                if (song.artist != album_artist) {
+                    return <li>{song.number}. {song.artist} - {song.name}</li>;
+                } else {
+                    return <li>{song.number}. {song.name}</li>;
+                }
+            }
+        );
+    } else {
+        //
+    }
+
+    return (
+        <ul style={{listStyleType: "none"}}>{ listSongs }</ul>
+    )
+}
+Songs.propTypes = {
+    discs: PropTypes.array,
+    album_artist: PropTypes.string
 }
 
 export default App;
