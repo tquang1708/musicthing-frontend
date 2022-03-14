b7635267bbd222d8602d87c0377e0e47d01f205d
Quang Tran <tquang1708@gmail.com>
2022-03-14 15:49:45 -0400

    add reload buttons

--- src/App.js
+++ src/App.js
@@ -1,54 +1,111 @@
 import { React, useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import './App.css';
+import { options } from './options.js';
 
 function App() {
     const [albumsState, setAlbumsState] = useState([]);
 
     useEffect(() => {
-        fetch('http://127.0.0.1:8000/api/list')
+        fetch(`${options.server_url}/api/list`)
             .then((response) => response.json())
             .then((data) => setAlbumsState(data.albums))
             .catch((error) => console.log(error));
     }, []);
 
     const listAlbums = albumsState.map((album) =>
-        <div key={album.name}>
-            <h3>{album.name}</h3>
-            <p>by {album.album_artist_name}</p>
-            <Songs discs={album.discs} album_artist={album.album_artist_name} />
+        <div key={album.name + album.album_artist_name}>
+            <h2>{album.name}</h2>
+            <h3>{album.album_artist_name}</h3>
+            <Discs discs={album.discs} album_artist={album.album_artist_name}  album_name = {album.name}/>
+        </div>
+    );
+
+    const reload = () => {
+        fetch(`${options.server_url}/api/reload`)
+            .then((response) => response.text())
+            .then(() => location.reload())
+            .catch((error) => console.log(error));
+    }
+
+    const hard_reload = () => {
+        fetch(`${options.server_url}/api/hard_reload`)
+            .then((response) => response.text())
+            .then(() => location.reload())
+            .catch((error) => console.log(error));
+    }
+
+    return (
+        <div>
+            <button onClick={reload}>Reload Metadata DB</button>
+            <button onClick={hard_reload}>Hard-Reload Metadata DB</button>
+            { listAlbums }
+        </div>
+    );
+}
+
+function Discs(props) {
+    const { discs, album_artist, album_name } = props;
+
+    const listDiscs = discs.map((disc) =>
+        <div key={album_name + album_artist + disc.number}>
+            <h5>Disc {disc.number}</h5>
+            <Songs album_artist={album_artist} tracks={disc.tracks} />
         </div>
     );
 
     return (
-        <div>{ listAlbums }</div>
+        <div>{ listDiscs }</div>
     );
+}
+Discs.propTypes = {
+    discs: PropTypes.array,
+    album_artist: PropTypes.string,
+    album_name: PropTypes.string
 }
 
 function Songs(props) {
-    const { discs, album_artist } = props;
-
-    let listSongs;
-    if (discs.length == 1) {
-        listSongs = discs[0].tracks.map((song) => {
-                if (song.artist != album_artist) {
-                    return <li>{song.number}. {song.artist} - {song.name}</li>;
-                } else {
-                    return <li>{song.number}. {song.name}</li>;
-                }
-            }
-        );
-    } else {
-        //
-    }
-
-    return (
-        <ul style={{listStyleType: "none"}}>{ listSongs }</ul>
+    const { album_artist, tracks } = props;
+    
+    const listTracks = tracks.map((track) =>
+        <div key={track.number + track.artist + track.name}>
+            <Song album_artist={album_artist} track={track} />
+        </div>
     )
+
+    return (<div>{ listTracks }</div>);
 }
 Songs.propTypes = {
-    discs: PropTypes.array,
-    album_artist: PropTypes.string
+    album_artist: PropTypes.string,
+    tracks: PropTypes.array
+}
+
+function Song(props) {
+    const { album_artist, track } = props;
+
+    let text;
+    if (album_artist == track.artist) {
+        text = `${track.name}`
+    } else {
+        text = `${track.artist} - ${track.name}`
+    }
+
+    const url = `${options.server_url}/static` + track.path;
+    const filename = track.path.split("/").pop();
+    return (
+    <div>
+        {track.number + ". "}
+        <a 
+            href = {url}
+            download = {filename}
+            target="_blank" rel="noreferrer">
+            {text}
+        </a>
+    </div>);
+}
+Song.propTypes = {
+    album_artist: PropTypes.string,
+    track: PropTypes.map
 }
 
 export default App;
---
+++ src/options.js
@@ -0,0 +1,3 @@
+export const options = {
+    "server_url": "http://localhost:8000",
+}
